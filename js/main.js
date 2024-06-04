4ff6aad722af6f69c0014c07bb194a66459f4adb
nnmhd <natchanone@gmail.com>
2024-06-04 09:34:07 -0400

    JS updated
    In this commit, JS was updated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
 // console.log("JS file connected");
-
-// const iconControl = document.querySelectorAll("#container object");
-// console.log(iconControl);
-
-// function logId() {
-//   console.log(this.id);
-// }
-
-// iconControl.forEach(function (object) {
-//   object.addEventListener("mouseover", logId);
-// });
