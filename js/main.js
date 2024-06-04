f5c8cfb4a40120681645097dc363e91c745bba9f
nnmhd <natchanone@gmail.com>
2024-06-04 09:32:41 -0400

    Script fixed
    In this commit, the JS script changed to an internal script in SVG.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-console.log("JS file connected");
+// console.log("JS file connected");
 
-const iconControl = document.querySelectorAll("#container object");
-console.log(iconControl);
+// const iconControl = document.querySelectorAll("#container object");
+// console.log(iconControl);
 
-function logId() {
-  console.log(this.id);
-}
+// function logId() {
+//   console.log(this.id);
+// }
 
-iconControl.forEach(function (object) {
-  object.addEventListener("mouseover", logId);
-});
-
-
+// iconControl.forEach(function (object) {
+//   object.addEventListener("mouseover", logId);
+// });
